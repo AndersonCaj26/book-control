a5aaaa12a1e14e0bd7fbaf88d679814caf75c1e3
AndersonCaj26 <andersoncaj26@gmail.com>
2010-05-01 01:47:11 -0300

    setting the view engine and creating the login page

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 
 //settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 
 //routers
 app.get('/', (req, res) => {
-    res.status(400).send('Ok! It´s working!');
+    res.status(400).render('login');
 });
 
 //activating server
